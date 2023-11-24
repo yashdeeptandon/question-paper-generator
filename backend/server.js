695172f0345a78dd20a3a1ab6610fdf9f9f22d84
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2023-11-24 23:39:29 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const questionRoutes = require("./routes/questionRoutes");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -10,13 +11,13 @@ const app = express(); // Initialize the express app here
 app.use(express.json());
 
 // Set up CORS after initializing app
-const corsOptions = {
-  origin: "https://question-paper-generator-seven.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "https://question-paper-generator-seven.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
+app.use(cors);
 
 // Connect to MongoDB
 connectDB();
@@ -24,7 +25,6 @@ connectDB();
 // Routes
 app.use("/api", questionRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
