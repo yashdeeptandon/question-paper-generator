86ef5b5995744fead3c0314f8305a5650dc507fd
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2024-09-15 22:31:53 +0530

    under maintenance state

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { Button } from "../components/ui/button";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainScreen: React.FC = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    toast.warning("This site is Under Maintenence");
+  }, []);
+
   return (
     <main className="w-full h-full flex justify-center">
+      <ToastContainer />
       <section className="w-full max-w-lg flex flex-col mx-auto p-4">
         <div className="text-[32px]">Generate Question Paper</div>
         <br />
